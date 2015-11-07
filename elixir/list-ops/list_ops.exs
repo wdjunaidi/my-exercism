66d1be67b7abdeefd7231829b76c2ace1edac26c
Wirianto Djunaidi <wirianto.djunaidi@gmail.com>
2015-11-08 07:36:27 +1300

    streamlined elixir/list_opts and redo append

--- elixir/list-ops/list_ops.exs
+++ elixir/list-ops/list_ops.exs
@@ -7,12 +7,8 @@ defmodule ListOps do
   # automatically imported) and so shouldn't be used either.
 
   @spec count(list) :: non_neg_integer
-  def count(l) do
-    _count(l)
-  end
-
-  defp _count([]), do: 0
-  defp _count([head|tail]), do: 1 + _count(tail)
+  def count([]), do: 0
+  def count([_|tail]), do: 1 + count(tail)
 
   @spec reverse(list) :: list
   def reverse(l) do
@@ -23,50 +19,29 @@ defmodule ListOps do
   defp _reverse([head | tail], acc), do: _reverse(tail, [head | acc])
 
   @spec map(list, (any -> any)) :: list
-  def map(l, f) do
-    _map(l, f)
-  end
-
-  defp _map([], _), do: []
-  defp _map([head|tail], f), do: [ f.(head) | _map(tail, f) ]
+  def map([], _), do: []
+  def map([head|tail], f), do: [ f.(head) | map(tail, f) ]
 
   @spec filter(list, (any -> as_boolean(term))) :: list
-  def filter(l, f) do
-    _filter(l, f)
-  end
-
-  defp _filter([], _), do: []
-  defp _filter([head|tail], f) do 
+  def filter([], _), do: []
+  def filter([head|tail], f) do 
     if f.(head) do
-      [ head | _filter(tail, f) ]
+      [ head | filter(tail, f) ]
     else
-      _filter(tail, f)
+      filter(tail, f)
     end
   end
 
   @type acc :: any
   @spec reduce(list, acc, ((any, acc) -> acc)) :: acc
-  def reduce(l, acc, f) do
-    _reduce(l, acc, f)
-  end
-
-  defp _reduce([], acc, _), do: acc
-  defp _reduce([head|tail], acc, f), do: f.(head, _reduce(tail, acc, f))
+  def reduce([], acc, _), do: acc
+  def reduce([head|tail], acc, f), do: f.(head, reduce(tail, acc, f))
 
   @spec append(list, list) :: list
-  def append(a, b) do
-    _append(b, a)
-  end
-
-  defp _append([], l), do: l
-  defp _append(l, []), do: l
-  defp _append(l, [head|tail]), do: [head | _append(l, tail)]
+  def append([], l), do: l
+  def append([head|tail], l), do: [head | append(tail, l)]
 
   @spec concat([[any]]) :: [any]
-  def concat(ll) do
-    _concat(ll)
-  end
-
-  defp _concat([]), do: []
-  defp _concat([head|tail]), do: append(head, concat(tail))
+  def concat([]), do: []
+  def concat([head|tail]), do: append(head, concat(tail))
 end
