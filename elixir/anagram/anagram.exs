ffe5899f12f9ed3ddf811af070fc6fc30b6dab60
Wirianto Djunaidi <wirianto.djunaidi@gmail.com>
2015-11-11 06:27:47 +1300

    commit working anagram

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -4,6 +4,23 @@ defmodule Anagram do
   """
   @spec match(String.t, [String.t]) :: [String.t]
   def match(base, candidates) do
-  
+    dbase = String.downcase(base)
+    Enum.filter(candidates, fn x -> anagram?(String.downcase(x), dbase) end)    
   end
+
+  defp anagram?(a, b) when length(a) != length(b), do: false
+  defp anagram?(a, b) do
+    if a == b do
+      false
+    else 
+      transform_word(a) == transform_word(b)
+    end
+  end
+  
+  defp transform_word(word) do
+    word
+    |> String.to_char_list
+    |> Enum.sort
+  end
+
 end
