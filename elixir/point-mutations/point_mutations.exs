cef737eb13a9336572052368e31069eafdcbf3de
Wirianto Djunaidi <wirianto.djunaidi@gmail.com>
2015-11-23 14:52:44 +1300

    add first working implementation of elixir/point-mutations

--- elixir/point-mutations/point_mutations.exs
+++ elixir/point-mutations/point_mutations.exs
@@ -8,6 +8,9 @@ defmodule DNA do
   4
   """
   @spec hamming_distance([char], [char]) :: non_neg_integer
-  def hamming_distance(strand1, strand2) do
-  end
+  def hamming_distance(l1, l2) when length(l1) != length(l2), do: nil
+  def hamming_distance([], []), do: 0
+  def hamming_distance([h1|t1], [h2|t2]) when h1 == h2, do: hamming_distance(t1, t2)
+  def hamming_distance([_|t1], [_|t2]), do: 1 + hamming_distance(t1, t2)
+
 end
--- elixir/point-mutations/point_mutations_test.exs
+++ elixir/point-mutations/point_mutations_test.exs
@@ -15,22 +15,22 @@ defmodule DNATest do
     assert DNA.hamming_distance('', '') == 0
   end
 
-  @tag :pending
+  # @tag :pending
   test "no difference between identical strands" do
     assert DNA.hamming_distance('GGACTGA', 'GGACTGA') == 0
   end
 
-  @tag :pending
+  # @tag :pending
   test "small hamming distance in middle somewhere" do
     assert DNA.hamming_distance('GGACG', 'GGTCG') == 1
   end
 
-  @tag :pending
+  # @tag :pending
   test "larger distance" do
     assert DNA.hamming_distance('ACCAGGG', 'ACTATGG') == 2
   end
 
-  @tag :pending
+  # @tag :pending
   test "hamming distance is undefined for strands of different lengths" do
     assert DNA.hamming_distance('AAAC', 'TAGGGGAGGCTAGCGGTAGGAC') == nil
     assert DNA.hamming_distance('GACTACGGACAGGACACC', 'GACATCGC') == nil
