f4baa17a8daeaa909becf3bf3381feef527c2664
Wirianto Djunaidi <wirianto.djunaidi@gmail.com>
2015-12-04 10:54:27 +1300

    modify point_mutations.exs to use Enum

--- elixir/point-mutations/point_mutations.exs
+++ elixir/point-mutations/point_mutations.exs
@@ -8,11 +8,10 @@ defmodule DNA do
   4
   """
   @spec hamming_distance([char], [char]) :: non_neg_integer
-  def hamming_distance(l1, l2), do: count_hamming_distance(l1, l2, 0)
-
-  defp count_hamming_distance(l1, l2, acc) when length(l1) != length(l2), do: nil
-  defp count_hamming_distance([], [], acc), do: acc
-  defp count_hamming_distance([h|t1], [h|t2], acc), do: count_hamming_distance(t1, t2, acc)
-  defp count_hamming_distance([_|t1], [_|t2], acc), do: count_hamming_distance(t1, t2, acc + 1)
+  def hamming_distance(l1, l2) when length(l1) != length(l2), do: nil
+  def hamming_distance(l1, l2) do
+    Enum.zip(l1, l2)
+    |> Enum.count(fn({n, n}) -> false; (_) -> true end)
+  end
 
 end
