d560b81870345289633349138f2911c2ab23e0d3
Wirianto Djunaidi <wirianto.djunaidi@gmail.com>
2015-12-04 10:35:54 +1300

    modify the hamming_distance() when h1 == h2 guard clause to use same variable name utilizing pattern matching

--- elixir/point-mutations/point_mutations.exs
+++ elixir/point-mutations/point_mutations.exs
@@ -10,7 +10,7 @@ defmodule DNA do
   @spec hamming_distance([char], [char]) :: non_neg_integer
   def hamming_distance(l1, l2) when length(l1) != length(l2), do: nil
   def hamming_distance([], []), do: 0
-  def hamming_distance([h1|t1], [h2|t2]) when h1 == h2, do: hamming_distance(t1, t2)
+  def hamming_distance([h|t1], [h|t2]), do: hamming_distance(t1, t2)
   def hamming_distance([_|t1], [_|t2]), do: 1 + hamming_distance(t1, t2)
 
 end
