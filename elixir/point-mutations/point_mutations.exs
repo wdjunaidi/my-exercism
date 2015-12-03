07dc7f99bc32c2d3b3a0c042125a334d6799700d
Wirianto Djunaidi <wirianto.djunaidi@gmail.com>
2015-12-04 10:41:15 +1300

    modify point_mutations to use tail-recursion

--- elixir/point-mutations/point_mutations.exs
+++ elixir/point-mutations/point_mutations.exs
@@ -8,9 +8,11 @@ defmodule DNA do
   4
   """
   @spec hamming_distance([char], [char]) :: non_neg_integer
-  def hamming_distance(l1, l2) when length(l1) != length(l2), do: nil
-  def hamming_distance([], []), do: 0
-  def hamming_distance([h|t1], [h|t2]), do: hamming_distance(t1, t2)
-  def hamming_distance([_|t1], [_|t2]), do: 1 + hamming_distance(t1, t2)
+  def hamming_distance(l1, l2), do: count_hamming_distance(l1, l2, 0)
+
+  defp count_hamming_distance(l1, l2, acc) when length(l1) != length(l2), do: nil
+  defp count_hamming_distance([], [], acc), do: acc
+  defp count_hamming_distance([h|t1], [h|t2], acc), do: count_hamming_distance(t1, t2, acc)
+  defp count_hamming_distance([_|t1], [_|t2], acc), do: count_hamming_distance(t1, t2, acc + 1)
 
 end
