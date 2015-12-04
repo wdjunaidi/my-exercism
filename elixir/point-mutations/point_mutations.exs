1ce1aa7501cf38d3039fa58a56f28132530717fb
Wirianto Djunaidi <wirianto.djunaidi@gmail.com>
2015-12-04 13:09:05 +1300

    streamline the anonymous function to check truthy for Enum.count in hamming_distance

--- elixir/point-mutations/point_mutations.exs
+++ elixir/point-mutations/point_mutations.exs
@@ -11,7 +11,7 @@ defmodule DNA do
   def hamming_distance(l1, l2) when length(l1) != length(l2), do: nil
   def hamming_distance(l1, l2) do
     Enum.zip(l1, l2)
-    |> Enum.count(fn({n, n}) -> false; (_) -> true end)
+    |> Enum.count(fn({n1, n2}) -> n1 != n2 end)
   end
 
 end
