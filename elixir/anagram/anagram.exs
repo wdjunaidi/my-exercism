7720662886770eb4250c021cdb116817e9d9e178
Wirianto Djunaidi <wirianto.djunaidi@gmail.com>
2017-01-10 08:06:59 +1300

    Add `@spec` to _anagram.exs_

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -5,15 +5,17 @@ defmodule Anagram do
   @spec match(String.t, [String.t]) :: [String.t]
   def match(base, candidates) do
     dbase = String.downcase(base)
-    Enum.filter(candidates, fn x -> anagram?(String.downcase(x), dbase) end)    
+    Enum.filter(candidates, fn x -> anagram?(String.downcase(x), dbase) end)
   end
 
+  @spec anagram?(String.t, String.t) :: boolean
   defp anagram?(a, b) when length(a) != length(b), do: false
   defp anagram?(a, b) when a == b, do: false
   defp anagram?(a, b) do
     transform_word(a) == transform_word(b)
   end
-  
+
+  @spec transform_word(String.t) :: String.t
   defp transform_word(word) do
     word
     |> String.to_char_list
