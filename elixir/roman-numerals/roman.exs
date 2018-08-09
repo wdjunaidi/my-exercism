368cb16bf47d7ad6e6671e0266e312ca8ef6ca91
Wirianto Djunaidi <wirianto.djunaidi@gmail.com>
2018-08-10 10:23:09 +1200

    Refactor elixir/roman-numeral_ solution

--- elixir/roman-numerals/roman.exs
+++ elixir/roman-numerals/roman.exs
@@ -5,20 +5,21 @@ defmodule Roman do
   @spec numerals(pos_integer) :: String.t()
   def numerals(number) do
     cond do
-      number >= 1000 -> "M" <> numerals(number - 1000)
-      div(number, 100) == 9 -> "CM" <> numerals(number - 900)
-      div(number, 100) >= 5 -> "D" <> numerals(number - 500)
-      div(number, 100) == 4 -> "CD" <> numerals(number - 400)
-      div(number, 100) >= 1 -> "C" <> numerals(number - 100)
-      div(number, 10) == 9 -> "XC" <> numerals(number - 90)
-      div(number, 10) >= 5 -> "L" <> numerals(number - 50)
-      div(number, 10) == 4 -> "XL" <> numerals(number - 40)
-      div(number, 10) >= 1 -> "X" <> numerals(number - 10)
-      div(number, 1) == 9 -> "IX" <> numerals(number - 9)
-      div(number, 1) >= 5 -> "V" <> numerals(number - 5)
-      div(number, 1) == 4 -> "IV" <> numerals(number - 4)
-      div(number, 1) >= 1 -> "I" <> numerals(number - 1)
+      number >= 1000        -> "M" <> numerals(number - 1000)
+      div(number, 100) >= 1 -> translate(number, 100, %{9 => "CM", 5 => "D", 4 => "CD", 1 => "C"})
+      div(number,  10) >= 1 -> translate(number,  10, %{9 => "XC", 5 => "L", 4 => "XL", 1 => "X"})
+      div(number,   1) >= 1 -> translate(number,   1, %{9 => "IX", 5 => "V", 4 => "IV", 1 => "I"})
       true -> ""
+    end
+  end
+
+  defp translate(number, scale, convertmap) do
+    cond do
+      div(number, scale) == 9 -> convertmap[9] <> numerals(number - (9 * scale))
+      div(number, scale) >= 5 -> convertmap[5] <> numerals(number - (5 * scale))
+      div(number, scale) == 4 -> convertmap[4] <> numerals(number - (4 * scale))
+      div(number, scale) >= 1 -> convertmap[1] <> numerals(number - (1 * scale))
+      true -> numerals(number)
     end
   end
 
