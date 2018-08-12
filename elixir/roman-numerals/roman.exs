201ec160d6df763e0a236e71418da2ef90490ad2
Wirianto Djunaidi <wirianto.djunaidi@gmail.com>
2018-08-13 06:30:42 +1200

    Move the roman digit map to constant

--- elixir/roman-numerals/roman.exs
+++ elixir/roman-numerals/roman.exs
@@ -1,4 +1,9 @@
 defmodule Roman do
+
+  @hundredth %{9 => "CM", 5 => "D", 4 => "CD", 1 => "C"}
+  @tenth %{9 => "XC", 5 => "L", 4 => "XL", 1 => "X"}
+  @nth %{9 => "IX", 5 => "V", 4 => "IV", 1 => "I"}
+
   @doc """
   Convert the number to a roman number.
   """
@@ -6,9 +11,9 @@ defmodule Roman do
   def numerals(number) do
     cond do
       number >= 1000        -> "M" <> numerals(number - 1000)
-      div(number, 100) >= 1 -> translate(number, 100, %{9 => "CM", 5 => "D", 4 => "CD", 1 => "C"})
-      div(number,  10) >= 1 -> translate(number,  10, %{9 => "XC", 5 => "L", 4 => "XL", 1 => "X"})
-      div(number,   1) >= 1 -> translate(number,   1, %{9 => "IX", 5 => "V", 4 => "IV", 1 => "I"})
+      div(number, 100) >= 1 -> translate(number, 100, @hundredth)
+      div(number,  10) >= 1 -> translate(number,  10, @tenth)
+      div(number,   1) >= 1 -> translate(number,   1, @nth)
       true -> ""
     end
   end
