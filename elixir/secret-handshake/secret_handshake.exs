a230d715e6695e5cdcabcfd432f7ec6910ad6c2e
Wirianto Djunaidi <wirianto.djunaidi@gmail.com>
2018-08-22 09:11:09 +1200

    commit elixir/secret-handshake

--- elixir/secret-handshake/secret_handshake.exs
+++ elixir/secret-handshake/secret_handshake.exs
@@ -1,4 +1,8 @@
 defmodule SecretHandshake do
+  use Bitwise, only_operators: true
+
+  @decoder %{8 => "jump", 4 => "close your eyes", 2 => "double blink", 1 => "wink" }
+
   @doc """
   Determine the actions of a secret handshake based on the binary
   representation of the given `code`.
@@ -15,5 +19,15 @@ defmodule SecretHandshake do
   """
   @spec commands(code :: integer) :: list(String.t())
   def commands(code) do
+    result = @decoder
+      |> Enum.map(fn({k,v}) -> if((k &&& code) == k, do: v) end)
+      |> Enum.filter(&(!is_nil(&1)))
+
+    if ((code &&& 16) == 16) do
+      Enum.reverse(result)
+    else
+      result
+    end
   end
+
 end
